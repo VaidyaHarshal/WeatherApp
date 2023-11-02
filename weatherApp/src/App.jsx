e65bd6daa0426c5e8e0f74a0998eb15f2f19499f
VaidyaHarshal <harshal.vaidya300@gmail.com>
2023-11-02 16:53:26 -0400

    Added static implementation of current weather along with icons

--- weatherApp/src/App.jsx
+++ weatherApp/src/App.jsx
@@ -1,4 +1,5 @@
 import Search from "./components/search/search";
+import CurrentWeather from "./components/currentWeather/CurrentWeather";
 import "./App.css";
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
   return (
     <div className="container">
       <Search onSearchChange={handleOnSearchChange} />
+      <CurrentWeather />
     </div>
   );
 }
