35c9eb0ec0be099744cca7fbbfc69b62bdf37cc7
VaidyaHarshal <harshal.vaidya300@gmail.com>
2023-11-03 19:19:54 -0400

    Fetching and mapping data from the weather api

--- weatherApp/src/api.jsx
+++ weatherApp/src/api.jsx
@@ -1,4 +1,6 @@
 export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo/cities";
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "d174d79ce31d61465c302e7164d19b92";
 export const geoApiOptions = {
   method: "GET",
   headers: {
